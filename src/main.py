f382c5af6b3bd91f595c1f2abe98e2df40c7f073
Alysson <alysson10araujo@gmail.com>
2021-10-05 19:37:44 -0300

    updade notificar videos

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import discord, random
+import discord, random, time, asyncio
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = "%", case_insensitive = True)
+client = commands.Bot(command_prefix = ">", case_insensitive = True)
 
 @client.event
-
 async def on_ready():
     print('Entramos como {0.user}' .format(client))
 
@@ -21,6 +20,25 @@ async def jogar_dado(ctx, number):
     random_number = random.randint(1,int(number))
     await ctx.send(f'O número que saiu do dado é {random_number}')
 
-
+### Notificar os vídeos de instalação do PostgreSQL
+@client.command()
+async def notificar_video(ctx):
+ count = 0 
+ while True:
+    count += 1
+    canal = client.get_channel(852273456108142632)
+    await asyncio.sleep(1) # em 24 horas ele notifica os usuários = 86400 segundos
+    await canal.send(f"""Olá alunos do <@&892512310042718208> , sejam bem vindos ao servidor do discord da """+
+                    """ disciplina Fundamentos de banco de dados! """+"\n"+"""A pedido da professora"""+
+                    """ Lívia, peço a todos que vejam estes vídeos nos quais explicamos como instalar"""+
+                    """ o PostgreSQL e o PGAdmin4. Em caso de dúvidas, fale conosco no canal de chat referente"""+
+                    """ a sua turma. Para isso basta nos mencionar com <@&842022732060557312> ou <@&842025228649627688>"""+
+                    """ nos canais de chat referentes a sua turma. Segue os links dos vídeos"""+
+                    """ abaixo:"""+"\n"+"https://youtu.be/f1J_iiNLp_M "+"\n"+"https://youtu.be/oilARCECtAg")
+    #await canal.send("https://youtu.be/f1J_iiNLp_M")
+    #await canal.send("https://youtu.be/oilARCECtAg")
+    break
+    
+    
 
 client.run('ODk0NDA0MjQ4MTg2NTI3ODI0.YVpg3Q.qH6lwaXZ3D0aWtf_Pjzy0Wz8mu8')
